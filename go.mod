9cc3c22f8e71df33006a035395d02e4cbb41d255
MarkusPersad <msp060308@gmail.com>
2025-03-05 18:49:13 +0800

    添加mariadb实例和valkiey实例

--- go.mod
+++ go.mod
@@ -10,10 +10,14 @@ require (
 	github.com/joho/godotenv v1.5.1
 	github.com/rs/zerolog v1.33.0
 	github.com/swaggo/swag v1.16.4
+	github.com/valkey-io/valkey-go v1.0.55
 	gopkg.in/yaml.v3 v3.0.1
+	gorm.io/driver/mysql v1.5.7
+	gorm.io/gorm v1.25.12
 )
 
 require (
+	filippo.io/edwards25519 v1.1.0 // indirect
 	github.com/KyleBanks/depth v1.2.1 // indirect
 	github.com/andybalholm/brotli v1.1.1 // indirect
 	github.com/asaskevich/govalidator v0.0.0-20230301143203-a9d515a09cc2 // indirect
@@ -29,9 +33,12 @@ require (
 	github.com/go-openapi/strfmt v0.23.0 // indirect
 	github.com/go-openapi/swag v0.23.0 // indirect
 	github.com/go-openapi/validate v0.24.0 // indirect
+	github.com/go-sql-driver/mysql v1.9.0 // indirect
 	github.com/gofiber/schema v1.3.0 // indirect
 	github.com/gofiber/utils/v2 v2.0.0-beta.7 // indirect
 	github.com/google/uuid v1.6.0 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/josharian/intern v1.0.0 // indirect
 	github.com/klauspost/compress v1.18.0 // indirect
 	github.com/lufia/plan9stats v0.0.0-20250303091104-876f3ea5145d // indirect
@@ -56,5 +63,5 @@ require (
 	golang.org/x/sync v0.11.0 // indirect
 	golang.org/x/sys v0.30.0 // indirect
 	golang.org/x/text v0.22.0 // indirect
-	golang.org/x/tools v0.22.0 // indirect
+	golang.org/x/tools v0.30.0 // indirect
 )
---
+++ internal/database/database.go
@@ -0,0 +1,181 @@
+package database
+
+import (
+	"context"
+	"fmt"
+	"maps"
+	"os"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/rs/zerolog/log"
+	"github.com/valkey-io/valkey-go"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"gorm.io/gorm/schema"
+)
+
+var (
+	dbInstance *service
+	dbUser string = os.Getenv("DB_USERNAME")
+	dbPassword string = os.Getenv("DB_PASSWORD")
+	dbHost string = os.Getenv("DB_HOST")
+	dbPort string = os.Getenv("DB_PORT")
+	dbName string = os.Getenv("DB_NAME")
+	maxOpenConns, _  = strconv.Atoi(os.Getenv("DB_MAX_OPEN_CONNS"))
+	maxIdleConns, _  = strconv.Atoi(os.Getenv("DB_MAX_IDLE_CONNS"))
+	dbMaxLifetime, _ = strconv.Atoi(os.Getenv("DB_CONN_MAX_LIFETIME"))
+	isSingularTable, _ = strconv.ParseBool(os.Getenv("DB_SINGULAR_TABLE"))
+	valHost          = os.Getenv("VALKEY_CLIENT_HOST")
+	valPort          = os.Getenv("VALKEY_CLIENT_PORT")
+	valPass          = os.Getenv("VALKEY_CLIENT_PASSWORD")
+	ctxTxKey         = "Tx"
+)
+
+type Service interface {
+	Health() map[string]string
+	Close() error
+	GetDB(ctx context.Context) *gorm.DB
+	Transaction(ctx context.Context, fn func(ctx context.Context) error) error
+}
+
+type service struct {
+	db *gorm.DB
+	valkey valkey.Client
+}
+
+func New() *service {
+	if dbInstance != nil {
+		return dbInstance
+	}
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
+		dbUser,dbPassword,dbHost,dbPort,dbName)
+	db,err := gorm.Open(mysql.Open(dsn),&gorm.Config{
+		NamingStrategy: schema.NamingStrategy{
+			SingularTable: isSingularTable,
+		},
+		SkipDefaultTransaction: true,
+	})
+	if err != nil {
+			log.Logger.Fatal().Err(err).Msg("failed to connect to database")
+	}
+	sqlDB, err := db.DB()
+	if err != nil {
+		log.Logger.Fatal().Err(err).Msg("failed to get sql db")
+		os.Exit(2)
+	}
+	sqlDB.SetMaxOpenConns(maxOpenConns)
+	sqlDB.SetMaxIdleConns(maxIdleConns)
+	sqlDB.SetConnMaxLifetime(time.Duration(dbMaxLifetime) * time.Second)
+
+	valClient, err := valkey.NewClient(valkey.ClientOption{
+			InitAddress: []string{valHost + ":" + valPort},
+			Password:    valPass,
+		})
+		if err != nil {
+			log.Logger.Fatal().Err(err).Msg("failed to create valkey client")
+			os.Exit(2)
+		}
+		dbInstance = &service{
+			db: db,
+			valkey: valClient,
+		}
+		return dbInstance
+}
+
+func (s *service) Health() map[string]string {
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	stats := make(map[string]string)
+	sqlDB, err := s.db.DB()
+	if err != nil {
+		stats["mariadb_status"] = "down"
+		stats["mariadb_error"] = fmt.Sprintf("mariadb down: %v", err)
+		log.Logger.Fatal().Err(err).Msg("mariadb down")
+		return stats
+	}
+	if err := sqlDB.PingContext(ctx); err != nil {
+		stats["mariadb_status"] = "down"
+		stats["mariadb_error"] = fmt.Sprintf("mariadb down: %v", err)
+		log.Logger.Fatal().Err(err).Msg("mariadb down")
+		return stats
+	}
+	stats["mariadb_status"] = "up"
+	stats["mariadb_message"] = "It's healthy"
+
+	dbStats := sqlDB.Stats()
+	stats["mariadb_open_connections"] = strconv.Itoa(dbStats.OpenConnections)
+	stats["mariadb_in_use"] = strconv.Itoa(dbStats.InUse)
+	stats["mariadb_idle"] = strconv.Itoa(dbStats.Idle)
+	stats["mariadb_wait_count"] = strconv.FormatInt(dbStats.WaitCount, 10)
+	stats["mariadb_wait_duration"] = dbStats.WaitDuration.String()
+	stats["mariadb_max_idle_closed"] = strconv.FormatInt(dbStats.MaxIdleClosed, 10)
+	stats["mariadb_max_lifetime_closed"] = strconv.FormatInt(dbStats.MaxLifetimeClosed, 10)
+	if dbStats.OpenConnections > 40 { // Assuming 50 is the max for this example
+		stats["mariadb_message"] = "The database is experiencing heavy load."
+	}
+	if dbStats.WaitCount > 1000 {
+		stats["mariadb_message"] = "The database has a high number of wait events, indicating potential bottlenecks."
+	}
+
+	if dbStats.MaxIdleClosed > int64(dbStats.OpenConnections)/2 {
+		stats["mariadb_message"] = "Many idle connections are being closed, consider revising the connection pool settings."
+	}
+
+	if dbStats.MaxLifetimeClosed > int64(dbStats.OpenConnections)/2 {
+		stats["mariadb_message"] = "Many connections are being closed due to max lifetime, consider increasing max lifetime or revising the connection usage pattern."
+	}
+	valResult := s.valkey.Do(ctx, s.valkey.B().Ping().Build())
+	if valResult.Error() != nil {
+		stats["valkey_status"] = "down"
+		stats["valkey_error"] = fmt.Sprintf("valkey down: %v", valResult.Error())
+		log.Logger.Fatal().Err(valResult.Error()).Msg("valkey down")
+		return stats
+	}
+	stats["valkey_status"] = "up"
+	stats["valkey_message"] = "It's healthy"
+	valStatus := parseValkeyInfo(valResult.String())
+	maps.Copy(stats, valStatus)
+	return stats
+}
+func parseValkeyInfo(info string) map[string]string {
+	result := make(map[string]string)
+	lines := strings.Split(info, "\r\n")
+	for _, line := range lines {
+		if strings.Contains(line, ":") {
+			parts := strings.Split(line, ":")
+			key := strings.TrimSpace(parts[0])
+			value := strings.TrimSpace(parts[1])
+			result[key] = value
+		}
+	}
+	return result
+}
+
+func (s *service) Close() error {
+	sqlDB, _ := s.db.DB()
+	log.Logger.Info().Msgf("Disconnected from %s database", dbName)
+	s.valkey.Close()
+	log.Logger.Info().Msgf("Disconnected from %s valkey", valHost)
+	return sqlDB.Close()
+}
+
+// GetDB return tx
+// If you need to create a Transaction, you must call DB(ctx) and Transaction(ctx,fn)
+func (s *service) GetDB(ctx context.Context) *gorm.DB {
+	if ctx != nil {
+		if tx, ok := ctx.Value(ctxTxKey).(*gorm.DB); ok {
+			return tx
+		}
+		return s.db.WithContext(ctx)
+	}
+	return s.db
+}
+
+func (s *service) Transaction(ctx context.Context, fn func(ctx context.Context) error) error {
+	return s.db.WithContext(ctx).Transaction(func(tx *gorm.DB) error {
+		ctx = context.WithValue(ctx, ctxTxKey, tx)
+		return fn(ctx)
+	})
+}
